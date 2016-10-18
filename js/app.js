1206646a5a6e8c5b698adfe21db121fb5eb73a2f
Victor Carpenter <victorcarpenter123@gmail.com>
2016-10-18 18:36:24 -0400

    edited ajax object

--- js/app.js
+++ js/app.js
@@ -2,6 +2,18 @@ var crntLat = ''
 var crntLong = ''
 var myWeatherKey = '0422dca222e2b33cd7a57d8e09a94ba2/'
 
+$.ajax({
+
+    url: 'https://www.googleapis.com/moderator/v1/series?key='+key,
+    data: myData,
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+    success: function() { alert("Success"); },
+    error: function() { alert('Failed!'); },
+    beforeSend: setHeader
+});
+
 
 
 
@@ -10,7 +22,7 @@ navigator.geolocation.getCurrentPosition(function getMyWeather(pos){
 
    crntLat = pos.coords.latitude
    crntLong = pos.coords.latitude
-   
+
    $.getJSON('https://api.darksky.net/forecast/' + myWeatherKey + crntLat + ',' + crntLong , function(forecast){
       console.log(forecast)
    })
