4197598f4117553d90e2f2df7a80ee18d40b47cc
Victor Carpenter <victorcarpenter123@gmail.com>
2016-10-18 18:24:33 -0400

    test

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
+var crntLat = ''
+var crntLong = ''
+var myWeatherKey = '0422dca222e2b33cd7a57d8e09a94ba2/'
+
+
+
 
 
 navigator.geolocation.getCurrentPosition(function getMyWeather(pos){
-   console.log(pos)
-   console.log(pos.coords.longitude)
+
+   crntLat = pos.coords.latitude
+   crntLong = pos.coords.latitude
 });
+
+$.getJSON('https://api.darksky.net/forecast/' + myWeatherKey + crntLat + ',' + crntLong , function(forecast){
+
+})
